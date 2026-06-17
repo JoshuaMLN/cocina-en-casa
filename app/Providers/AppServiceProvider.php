6bc887f51cf080b19133bcf0aca4e5d2bf6a6582
JoshuaMLN <joshua.leon@unmsm.edu.pe>
2026-06-17 17:45:31 -0500

    [fix] Problemas con host sql forzando string length a 191 en lugar de 255

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
+
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
